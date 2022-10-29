6d2263b4b8486a667d0613731a1931ff584b7264
Loly Maluf <lolymaluf@gmail.com>
2022-10-29 18:38:11 -0300

    Adiciona Requisitos 01,14 e 18 Migrations e Models

---
+++ app/backend/src/database/models/MatchesModel.ts
@@ -0,0 +1,59 @@
+import { Model, INTEGER, BOOLEAN } from 'sequelize';
+import db from '.';
+import Teams from './TeamsModel';
+
+class Matches extends Model {
+  declare id: number;
+  declare homeTeam: number;
+  declare homeTeamGoals: number;
+  declare awayTeam: number;
+  declare awayTeamGoals: number;
+  declare inProgress: boolean;
+}
+
+Matches.init({
+  id: {
+    type: INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  homeTeam: {
+    type: INTEGER,
+    allowNull: false,
+    field: 'home_team',
+  },
+  homeTeamGoals: {
+    type: INTEGER,
+    allowNull: false,
+    field: 'home_team_goals',
+  },
+  awayTeam: {
+    type: INTEGER,
+    allowNull: false,
+    field: 'away_team',
+  },
+  awayTeamGoals: {
+    type: INTEGER,
+    allowNull: false,
+    field: 'away_team_goals',
+  },
+  inProgress: {
+    type: BOOLEAN,
+    allowNull: false,
+    field: 'in_progress',
+  },
+}, {
+  underscored: true,
+  sequelize: db,
+  timestamps: false,
+  tableName: 'matches',
+});
+
+export default Matches;
+
+Teams.hasMany(Matches, { foreignKey: 'homeTeam', as: 'teamHome' });
+Teams.hasMany(Matches, { foreignKey: 'awayTeam', as: 'teamAway' });
+
+Matches.belongsTo(Teams, { foreignKey: 'homeTeam', as: 'teamHome' });
+Matches.belongsTo(Teams, { foreignKey: 'awayTeam', as: 'teamAway' });
---
+++ app/backend/src/database/models/TeamsModel.ts
@@ -0,0 +1,28 @@
+import { Model, INTEGER, STRING } from 'sequelize';
+import db from '.';
+
+class Teams extends Model {
+  declare id: number;
+  declare teamName: string;
+}
+
+Teams.init({
+  id: {
+    type: INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  teamName: {
+    type: STRING,
+    allowNull: false,
+    field: 'team_name',
+  },
+}, {
+  underscored: true,
+  sequelize: db,
+  timestamps: false,
+  tableName: 'teams',
+});
+
+export default Teams;
